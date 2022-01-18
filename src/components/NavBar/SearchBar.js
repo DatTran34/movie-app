4fe83304f99145666fa7bf7a842f93ad8e4413b1
DatTran34 <tridat1vn@gmail.com>
2022-01-18 18:33:45 -0500

    fixed search_bar

--- src/components/NavBar/SearchBar.js
+++ src/components/NavBar/SearchBar.js
@@ -7,102 +7,8 @@ import PersonIcon from "@mui/icons-material/Person";
 import TheatersIcon from "@mui/icons-material/Theaters";
 import TvIcon from "@mui/icons-material/Tv";
 import { useHistory } from "react-router";
+import SearchBarStyle from "../../styles/components/SearchBarStyle"
 import { makeStyles } from "@mui/styles";
-const SearchBarStyle = makeStyles((theme) => ({
-  navbar_search: {
-    position: "relative",
-    flex: "0",
-    display: "flex",
-    flexDirection: "row",
-    justifyContent: "flex-end",
-    color: "#CCD2E3",
-  },
-  navbar_searchBox: {
-    display: "flex",
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-    backgroundColor: "transparent",
-    // boxShadow: "inset 0px 4px 4px rgba(0, 0, 0, 0.25)",
-    border: "1px solid #CCD2E3",
-    borderRadius: "10px",
-    fontSize: "0.75rem",
-    transition: "width 0.5s",
-    padding: "0.5rem",
-    "$:hover": {
-      backgroundColor: "#CCD2E3",
-    },
-  },
-
-  navbar_searchBox_searching: {
-    backgroundColor: "#CCD2E3",
-    color: "#1f2845",
-    width: "100%",
-  },
-  navbar_input: {
-    background: "transparent",
-    // '&:placeholder-shown ': {
-    //     backgroundColor: "transparent",
-    // },
-    // '$navbar_input:hover ~$navbar_searchButton': {
-    //     display: "flex",
-    // },
-    border: "none",
-    outline: "none",
-  },
-
-  navbar_searchButton: {
-    display: "none",
-    cursor: "pointer",
-    padding: "0.25rem 1rem",
-    justifyContent: "center",
-    alignItems: "center",
-    borderRadius: "20px",
-    backgroundColor: "#374269",
-    transition: "all 1s ease-out",
-  },
-  navbar_searchButton_searching: {
-    display: "flex",
-  },
-  navbar_search_panel: {
-    position: "absolute",
-    top: "60px",
-    width: "100%",
-    background: "rgba(51, 45, 89, 0.6)",
-    boxShadow: "5px 4px 4px rgba(0, 0, 0, 0.25)",
-    webkitBackdropFilter: "blur(40px)",
-    backdropFilter: "blur(40px)",
-    transform: "translateX(-45)",
-    border: "none",
-    borderRadius: "5px",
-    color: "#ffffff",
-    display: "flex",
-    flexDirection: "column",
-  },
-  navbar_search_panel_item: {
-    padding: "10px",
-    borderBottom: "1px solid #9C9AA7",
-    display: "flex",
-    flexDirection: "row",
-    cursor: "pointer",
-    "&:hover": {
-      background: "#fbc108",
-    },
-  },
-  navbar_search_panel_last_item: {
-    padding: "10px",
-    display: "flex",
-    flexDirection: "row",
-    cursor: "pointer",
-  },
-  item: {
-    padding: "5px",
-  },
-  item_span: {
-    padding: "5px",
-    color: "#fb4e68",
-  },
-}));
 function SearchCard({ history, item, className_ }) {
   const searchBarStyle = SearchBarStyle();
   return (
@@ -159,6 +65,7 @@ function SearchBar() {
     [searchBarStyle.navbar_searchButton]: true,
   });
   const navbarSearchBoxStyle = classNames({
+    [searchBarStyle.navbar_searchBox_searching_scale_width]: isSearchShown,
     [searchBarStyle.navbar_searchBox_searching]: isSearchShown,
     [searchBarStyle.navbar_searchBox]: true,
   });
@@ -180,7 +87,7 @@ function SearchBar() {
     }
   };
   return (
-    <div className={searchBarStyle.navbar_search}>
+    <div className={searchBarStyle.navbar_search_1}>
       <Stack className={navbarSearchBoxStyle}>
         <Stack direction="row">
           <SearchIcon></SearchIcon>
@@ -195,7 +102,7 @@ function SearchBar() {
         <div className={navbarSearchButtonStyle}>Search</div>
       </Stack>
       {isSearchShown && (
-        <div className={searchBarStyle.navbar_search_panel}>
+        <div className={`${searchBarStyle.navbar_search_panel} ${searchBarStyle.navbar_search_panel_absolute}`}>
           {searchList.length === 0 ? (
             <div className={searchBarStyle.navbar_search_panel_last_item}>
               No Result
--- src/components/NavBar/SmallCategoryBar.js
+++ src/components/NavBar/SmallCategoryBar.js
@@ -1,6 +1,9 @@
 import { React, useState, useEffect } from "react";
 import { makeStyles } from "@mui/styles";
 import classNames from "classname";
+import SearchBar2 from "./SearchBar2";
+import { getCountries, getGenres } from "../../axios/NavBarRequest";
+import { useHistory, useLocation } from "react-router";
 const SmallsmallCategoryStyle = makeStyles((theme) => ({
   container: {
     position: " absolute",
@@ -34,14 +37,15 @@ const SmallsmallCategoryStyle = makeStyles((theme) => ({
   navbar_button: {
     cursor: "pointer",
     padding: "0.5rem 0 ",
+    borderRadius:"10px",
     "&:hover": {
-      background: "#CCD2E3",
-      color: "#1f2845"
+      background: "#4CCDEB",
+      color: "#ffffff"
     },
   },
   navbar_button_hover:{
-    background: "#CCD2E3",
-    color: "#1f2845"
+    background: "#4CCDEB",
+    color: "#ffffff"
 },
   panel: {
     display: "grid",
@@ -51,11 +55,11 @@ const SmallsmallCategoryStyle = makeStyles((theme) => ({
   panel_item: {
     cursor: "pointer",
     padding: "0.5rem 0 ",
-    background: "#CCD2E3",
-    color: "#1f2845",
   },
 }));
 function SmallCategoryBar() {
+  const history = useHistory();
+  const location = useLocation();
   const smallCategoryStyle = SmallsmallCategoryStyle();
   const [isMoviesShown, setIsMoviesShown] = useState(false);
   const [isTVShowsShown, setTVShowsShown] = useState(false);
@@ -90,50 +94,184 @@ const navbarCountryButtonStyle = classNames({
   [smallCategoryStyle.navbar_button_hover]: isCountryShown,
   [smallCategoryStyle.navbar_button]: true
 });
+//=====================GET Genre List=================//
+const [genres, setGenres] = useState([]);
+const [languages, setLanguages] = useState([]);
+const [filteredLanguages, setFilteredLanguages] = useState([]);
+useEffect(() => {
+  let searchParams = new URLSearchParams(location.search);
+  let mediaType = " ";
+  if (searchParams.get("media_type") === null) {
+    mediaType = "movie";
+  } else {
+    mediaType = searchParams.get("media_type");
+  }
+  getGenres(mediaType)
+    .then((data) => {
+      setGenres(data.genres);
+    })
+    .catch((e) => {
+      console.log(e);
+    });
+  getCountries()
+    .then((data) => {
+      setLanguages(data);
+    })
+    .catch((e) => {
+      console.log(e);
+    });
+  handleYearInNavBar();
+  handleSearchLanguage("");
+}, [location]);
 
+const handleSearchLanguage = (value) => {
+  if (value !== "") {
+    setIsSearchLanguageShown(true);
+    setFilteredLanguages(
+      languages.filter((language) => {
+        if (language.english_name.search(new RegExp(value, "i")) === -1)
+          return 0;
+        else return 1;
+      })
+    );
+  } else {
+    setIsSearchLanguageShown(false);
+  }
+};
+
+//============== GENERATE THE YEAR ===============
+const [yearArray, setYearArray] = useState([]);
+const handleYearInNavBar = () => {
+  var THE_NUMBER_OF_YEAR = 18;
+  var current_year = new Date().getFullYear();
+  var year_array_ = new Array();
+  for (var i = 0; i < THE_NUMBER_OF_YEAR; i++) {
+    year_array_.push(current_year - 1);
+    current_year = current_year - 1;
+  }
+  setYearArray(year_array_);
+};
+//*****************function to handle Query Params*********************/
+const addQuery = (key, value) => {
+  let pathname = location.pathname;
+  // returns path: '/app/books'
+  let searchParams = new URLSearchParams(location.search);
+  checkMediaType(searchParams);
+  // returns the existing query string: '?genre=fiction&year=2019'
+  searchParams.set(key, value);
+  searchParams.delete("category");
+  searchParams.delete("page");
+  history.push({
+    pathname: "/filter",
+    search: searchParams.toString(),
+  });
+};
+
+const addQuery_2 = (media_type, value) => {
+  let searchParams = new URLSearchParams(location.search);
+  searchParams.delete("genre");
+  searchParams.delete("year");
+  searchParams.delete("country");
+  searchParams.delete("page");
+  searchParams.set("media_type", media_type);
+  searchParams.set("category", value);
+  history.push({
+    pathname: "/filter",
+    search: searchParams.toString(),
+  });
+};
+
+const checkMediaType = (searchParams) => {
+  if (!searchParams.has("media_type")) {
+    searchParams.set("media_type", "movie");
+  }
+};
+
+const removeQuery = (key) => {
+  let pathname = location.pathname;
+  // returns path: '/app/books'
+  let searchParams = new URLSearchParams(location.search);
+  // returns the existing query string: '?type=fiction&author=fahid'
+  searchParams.delete(key);
+  history.push({
+    pathname: "/filter",
+    search: searchParams.toString(),
+  });
+};
   return (
     <div className={smallCategoryStyle.container}>
       <div style={{ padding: "0 1rem" }}>
-         <div className={smallCategoryStyle.box}>
-        <div className={smallCategoryStyle.overlay_outter}>
-          <div className={smallCategoryStyle.overlay_inner}>
-            <div className={smallCategoryStyle.col}>
-              <div
-                 className={navbarMoviesButtonStyle}
-                onClick={() => {
-                  setIsMoviesShown(!isMoviesShown);
-                }}
-              >
-                Movie
+        <div className={smallCategoryStyle.box}>
+          <div className={smallCategoryStyle.overlay_outter}>
+            <div className={smallCategoryStyle.overlay_inner}>
+              <div className={smallCategoryStyle.col}>
+                <SearchBar2 />
               </div>
-              {isMoviesShown && (
-                <div className={smallCategoryStyle.panel}>
-                  <div className={smallCategoryStyle.panel_item}>row</div>
-                  <div className={smallCategoryStyle.panel_item}>row</div>
-                  <div className={smallCategoryStyle.panel_item}>row</div>
-                  <div className={smallCategoryStyle.panel_item}>row</div>
-                  <div className={smallCategoryStyle.panel_item}>row</div>
-                </div>
-              )}
-            </div>
 
-            <div className={smallCategoryStyle.col}>col</div>
-            <div className={smallCategoryStyle.col}>col</div>
-            <div className={smallCategoryStyle.col}>col</div>
-            <div className={smallCategoryStyle.col}>col</div>
-            <div className={smallCategoryStyle.col}>col</div>
-            <div className={smallCategoryStyle.col}>col</div>
-            <div className={smallCategoryStyle.col}>col</div>
-            <div className={smallCategoryStyle.col}>col</div>
-            <div className={smallCategoryStyle.col}>col</div>
-            <div className={smallCategoryStyle.col}>col</div>
-            <div className={smallCategoryStyle.col}>col</div>
-            <div className={smallCategoryStyle.col}>col</div>
+              <div className={smallCategoryStyle.col}>
+                <div className={navbarMoviesButtonStyle}>
+                  <div
+                    onClick={() => {
+                      setIsMoviesShown(!isMoviesShown);
+                    }}
+                  >
+                    Movie
+                  </div>
+                  {isMoviesShown && (
+                    <div className={smallCategoryStyle.panel}>
+                      <div
+                        className={smallCategoryStyle.navbar_panel_item}
+                        onClick={() => {
+                          addQuery_2("movie", "Popular");
+                        }}
+                      >
+                        Popular
+                      </div>
+                      <div
+                        className={smallCategoryStyle.navbar_panel_item}
+                        onClick={() => {
+                          addQuery_2("movie", "Now Playing");
+                        }}
+                      >
+                        Now Playing
+                      </div>
+                      <div
+                        className={smallCategoryStyle.navbar_panel_item}
+                        onClick={() => {
+                          addQuery_2("movie", "Up Coming");
+                        }}
+                      >
+                        Up Coming
+                      </div>
+                      <div
+                        className={smallCategoryStyle.navbar_panel_item}
+                        onClick={() => {
+                          addQuery_2("movie", "Top Rated");
+                        }}
+                      >
+                        Top Rated
+                      </div>
+                    </div>
+                  )}
+                </div>
+              </div>
+
+              <div className={smallCategoryStyle.col}>col</div>
+              <div className={smallCategoryStyle.col}>col</div>
+              <div className={smallCategoryStyle.col}>col</div>
+              <div className={smallCategoryStyle.col}>col</div>
+              <div className={smallCategoryStyle.col}>col</div>
+              <div className={smallCategoryStyle.col}>col</div>
+              <div className={smallCategoryStyle.col}>col</div>
+              <div className={smallCategoryStyle.col}>col</div>
+              <div className={smallCategoryStyle.col}>col</div>
+              <div className={smallCategoryStyle.col}>col</div>
+              <div className={smallCategoryStyle.col}>col</div>
+              <div className={smallCategoryStyle.col}>col</div>
+            </div>
           </div>
         </div>
       </div>
-      </div>
-     
     </div>
   );
 }
